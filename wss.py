4b00a3f64e3125ad85f0c5112e60f08c234c62fe
zx <jhawdgawjh@163.com>
2024-09-22 13:52:50 +0800

    添加wss协议供前端读取

---
+++ wss.py
@@ -0,0 +1,36 @@
+import asyncio
+import websockets
+import json
+from future.kline.kliner import KlineService
+
+
+def get_tickets():
+    ks = KlineService()
+    tikcets = ks.load_ticket()
+    print(tikcets)
+
+
+
+async def send_data(websocket, path):
+    ks = KlineService()
+    # 等待接收客户端发送的消息
+    message = await websocket.recv()
+    # 判断是否是指定的消息
+    if message == "bind_tf_futures_trade":
+        # 循环发送数据
+        while True:
+            # 这里可以替换为你想要发送的数据
+            data = ks.load_ticket()
+            await websocket.send(json.dumps(data))
+            await asyncio.sleep(1)  # 每秒发送一次
+
+if __name__ == '__main__':
+    # get_tickets()
+
+
+
+    start_server = websockets.serve(send_data, '0.0.0.0', 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+
+    asyncio.get_event_loop().run_forever()
